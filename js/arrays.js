efc63d5d808cb447ee8153fd7e87f2ff9d9d124e
Sujay9550 <hardysujay@gmail.com>
2021-12-26 23:10:24 +0530

    Added Exercise Problems 3 to 5

--- js/arrays.js
+++ js/arrays.js
@@ -329,3 +329,41 @@ const sortedArray = (array) => {
 };
 
 console.log(sortedArray(stringArray)); // Result - ['CSS', 'HTML', 'JavaScript']
+
+// 3. You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [john, pete, mary];
+console.log(users);
+
+const usernames = users.map((arrayElement, index, array) => arrayElement.name);
+console.log(usernames); // Result - ['John', 'Pete', 'Mary']
+
+// 4: You have an array of user objects, each one has name, surname and id.
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+let john1 = { name: "John", surname: "Smith", id: 1 };
+let pete1 = { name: "Pete", surname: "Hunt", id: 2 };
+let mary1 = { name: "Mary", surname: "Key", id: 3 };
+
+let users1 = [john1, pete1, mary1];
+
+const usersFullName = users1.map((arrayElement, index, array) => ({
+  fullName: `${arrayElement.name} ${arrayElement.surname}`,
+  id: arrayElement.id,
+}));
+console.log(usersFullName);
+
+// 5: Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+let john2 = { name: "John", age: 25 };
+let pete2 = { name: "Pete", age: 30 };
+let mary2 = { name: "Mary", age: 28 };
+
+let users2 = [pete2, john2, mary2];
+
+const sortByAge = (users) => {
+  return users.sort((a, b) => a.age - b.age);
+};
+
+console.log(sortByAge(users2));
